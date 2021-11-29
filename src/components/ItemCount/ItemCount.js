a0b38922cbc13dc0e46d2fbff794f3af1499c04c
Matias Yogui <myogui@technisys.com>
2021-11-29 13:39:33 -0300

    agrego desafio sincronizar counter

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,9 +1,6 @@
 import Button from "@mui/material/Button";
-import { useCounter } from "../hooks/useCounter";
 
-export const ItemCount = ({ stock }) => {
-  const { counter, incrementar, decrementar } = useCounter(stock);
-
+export const ItemCount = ({ incrementar, decrementar, add, counter }) => {
   return (
     <div>
       <Button variant="outlined" onClick={decrementar}>
@@ -13,6 +10,11 @@ export const ItemCount = ({ stock }) => {
       <Button variant="outlined" onClick={incrementar}>
         +
       </Button>
+      <div>
+        <Button variant="contained" onClick={add}>
+          Agregar
+        </Button>
+      </div>
     </div>
   );
 };
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -2,12 +2,19 @@ import React from "react";
 import { ItemCount } from "../ItemCount/ItemCount";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@mui/material";
+import { useCounter } from "../hooks/useCounter";
 
 export const ItemDetail = ({ item }) => {
   const navigate = useNavigate();
 
+  const { counter, incrementar, decrementar } = useCounter(item.stock);
+
   const handleVolver = () => {
     navigate(-1);
+  };
+
+  const handleAgregar = () => {
+    console.log("item agregado", item.id, item.nombre, "cantidad: " + counter);
   };
 
   return (
@@ -16,7 +23,12 @@ export const ItemDetail = ({ item }) => {
       <h2>Producto: {item.nombre}</h2>
       <p>Precio: ${item.precio}</p>
       <p>Descripcion: {item.descripcion}</p>
-      <ItemCount stock={item.stock} />
+      <ItemCount
+        incrementar={incrementar}
+        decrementar={decrementar}
+        add={handleAgregar}
+        counter={counter}
+      />
 
       <Button onClick={handleVolver}>VOLVER</Button>
     </div>
